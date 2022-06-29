8915c43ff3078bade19cac90482e952fd2ac846e
Josephine Luu <josephinegluu@gmail.com>
2022-06-29 08:38:39 -0500

    commit tree 9b5eb34a9ac459c3d07c3b7edf66edef839f1b39

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//main.remove();
+document.querySelector('#main').remove();
+//# refers to the ID in html file, . refers to a class 
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+// to show in DOM use the follow code: document.body.append(newHeader);
